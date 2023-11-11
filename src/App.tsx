37b9b1d010f133b050476a18e287b1fcbbc98a82
Amir Hossein Peyvand <amirhosseinpeyvand2020@gmail.com>
2023-11-11 01:35:14 -0800

    Building the nav part

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import "./sass/App.scss";
+
 function App() {
-  return <></>;
+  return (
+    <div className="app">
+      <header>
+        <nav>
+          <div className="left">
+            <p>Notifications</p>
+            <span>3</span>
+          </div>
+          <div className="right">
+            <p>Mark all as read</p>
+          </div>
+        </nav>
+      </header>
+      <main></main>
+    </div>
+  );
 }
 
 export default App;
