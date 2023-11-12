a2a51ab32e7683d1d7b847e969de5119cfcbde1f
Amir Hossein Peyvand <amirhosseinpeyvand2020@gmail.com>
2023-11-12 05:58:19 -0800

    Basics of building notification component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Notification from "./components/Notification";
 import "./sass/App.scss";
+import markWebber from "./assets/avatar-mark-webber.webp";
 
 function App() {
   return (
@@ -9,7 +10,12 @@ function App() {
         <Navbar />
       </header>
       <main>
-        <Notification />
+        <Notification
+          fullName="Mark Webber"
+          profilePhoto={markWebber}
+          reactionText="reacted to your recent post My first tournament today!"
+          time="1m ago"
+        />
       </main>
     </div>
   );
--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,7 +1,7 @@
 import "../sass/Notification.scss";
 
 interface Props {
-  clubName?: string;
+  groupName?: string;
   commentText?: string;
   followText?: string;
   fullName: string;
@@ -10,14 +10,14 @@ interface Props {
   pic?: string;
   postName?: string;
   privateMessage?: string;
-  profilePhoto?: string;
+  profilePhoto: string;
   reactionText?: string;
   requestText?: string;
   time: string;
 }
 
 const Notification = ({
-  clubName,
+  groupName,
   commentText,
   followText,
   fullName,
@@ -31,7 +31,53 @@ const Notification = ({
   requestText,
   time,
 }: Props) => {
-  return <article className="notification"></article>;
+  const reaction = (
+    <>
+      {reactionText}
+      <strong className="postName" id="postName">
+        {postName}
+      </strong>
+    </>
+  );
+
+  const follow = <>{followText}</>;
+  const join = (
+    <>
+      {joinText} <strong className="group">{groupName}</strong>
+    </>
+  );
+  const request = <>{requestText}</>;
+  const comment = <>{commentText}</>;
+  const leave = <>{leaveText}</>;
+
+  return (
+    <article className="notification">
+      <section>
+        <picture className="profilePhotoContainer">
+          <img src={profilePhoto} alt={profilePhoto} />
+        </picture>
+        <div className="infoContainer">
+          <p>
+            <strong className="fullName">{fullName}</strong>
+            {reactionText
+              ? reaction
+              : followText
+              ? follow
+              : joinText
+              ? join
+              : requestText
+              ? request
+              : commentText
+              ? comment
+              : leave}
+          </p>
+          <span>{time}</span>
+          {privateMessage && <p className="privateMessage">{privateMessage}</p>}
+        </div>
+      </section>
+      {pic && <img src={pic} alt="pic" />}
+    </article>
+  );
 };
 
 export default Notification;
